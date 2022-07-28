f304ef0e61204ebac4486989eb829c9af4e37a84
praveen kumar <praveenatukuri888@gmial.com>
2022-07-28 10:00:57 +0000

    updated envirnoment

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_id" {
-    value = azurerm_resource_group.devops-rg.id
+  value = azurerm_resource_group.devops-rg.id
 }
 
 output "serviceplan_id" {
-    value = azurerm_app_service_plan.appserviceplan.id
+  value = azurerm_app_service_plan.appserviceplan.id
 }
 
 output "appservice_url" {
-    value = azurerm_app_service.aleti88-devops.default_site_hostname
+  value = azurerm_app_service.aleti88-devops.default_site_hostname
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "azurerm_resource_group_name" {}
 variable "azurerm_resource_group_location" {}
 variable "azurerm_app_service_plan_name" {}
-variable "Environment" {}
+variable "environment" {}
 variable "size" {}
 variable "azurerm_app_service_name" {}
 variable "app_service_plan_tier" {}
